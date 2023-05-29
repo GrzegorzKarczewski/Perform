2e235ed6ceb97630defb52d68e8487e16bbb8690
PrankSinatra <7526868+vorlonempire@users.noreply.github.com>
2023-05-29 16:39:47 +0200

    Simple, workable ui

--- Perform/MainPage.xaml.cs
+++ Perform/MainPage.xaml.cs
@@ -4,6 +4,7 @@ namespace Perform;
 
 public partial class MainPage : ContentPage
 {
+    public string CurrentName = String.Empty;
     private CsvDataViewModel viewModel;
     public MainPage()
     {
@@ -22,27 +23,53 @@ public partial class MainPage : ContentPage
     {
         foreach (var row in viewModel.Rows)
         {
-            var tableSection = new TableSection();
-            tableSection.Title = "Your Performace:";
-            tableSection.TextColor = Colors.Black;
+            string currentName = null;
+            string colliValue = null;
+            string totalcollies = null;
+
             foreach (var column in row)
             {
-                if (column.Key == "Naam_Medewerker" || column.Key == "Colli_Gem_Colli_Per_Uur")
+                if (column.Key == "Naam_Medewerker" && column.Value.ToUpper() == CurrentName)
                 {
-                    if (column.Value.ToUpper() == entryName.ToString())
-                    {
-                        tableSection.Add(new TextCell
-                        {
-                            Text = $"{column.Key}: {column.Value}",
-                            TextColor = Colors.Black
-
-                        });
-                    }
+                    currentName = $"Name: {column.Value}";
+                }
+                else if (column.Key == "Colli_Gem_Colli_Per_Uur")
+                {
+                    colliValue = $"Performance: {column.Value} colli/hr";
+                }
+                else if (column.Key == "Totaal_Colli_Gedaan")
+                {
+                    totalcollies = $"You've picked {column.Value} colli ";
                 }
             }
 
-            tableView.Root.Add(tableSection);
+            if (currentName != null && colliValue != null && totalcollies != null)
+            {
+                var tableSection = new TableSection();
+                tableSection.Title = "Your latest Performance:";
+                tableSection.TextColor = Colors.Black;
+
+                tableSection.Add(new TextCell
+                {
+                    Text = currentName,
+                    TextColor = Colors.Black,
+
+
+                });
+                tableSection.Add(new TextCell
+                {
+                    Text = colliValue,
+                    TextColor = Colors.Black
+                });
+                tableSection.Add(new TextCell
+                {
+                    Text = totalcollies.ToString(),
+                    TextColor = Colors.Black
+                });
+                tableView.Root.Add(tableSection);
+            }
         }
+
 
     }
 
@@ -50,16 +77,29 @@ public partial class MainPage : ContentPage
     {
         lblHello.Text = $"Hi {entryName.Text}";
         entryName.IsVisible = false;
-        entryName.Text.ToUpper();
+        entryName.Unfocus();
+        CurrentName = entryName.Text.ToUpper();
         lblEnterLogin.IsVisible = false;
-
+        loadingUserData.IsVisible = true;
         LoadDataAsync();
-        // loadingUserData.IsVisible = false;
+        entryName.IsVisible = false;
+        tableView.Focus();
+#if ANDROID
+        var imm = (Android.Views.InputMethods.InputMethodManager)MauiApplication.Current.GetSystemService(Android.Content.Context.InputMethodService);
+
+        if (imm != null)
+        {
+            //this stuff came from here: 
+            var activity = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity;
+            Android.OS.IBinder wToken = activity.CurrentFocus?.WindowToken;
+            imm.HideSoftInputFromWindow(wToken, 0);
+        }
+#endif
     }
 
     private void EntryName_TextChanged(object sender, TextChangedEventArgs e)
     {
-        // loadingUserData.IsVisible = true;
+        loadingUserData.IsVisible = true;
     }
 }
 
